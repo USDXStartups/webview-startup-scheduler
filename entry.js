06d174777a89b96a48ee6e41a3ee5a5203324a04
Tobias Wright <tobias.wright@gmail.com>
2016-09-13 11:09:24 -0700

    initial commit

---
+++ entry.js
@@ -0,0 +1,31 @@
+require("./style.css");
+$ = require("jquery");
+
+// console.log(require("./content.js"));
+
+var url = "https://startupcalendarhelper.azurewebsites.net/api/RequestTopicExpert?code=6yy62ob12opbsym3ombgkeudrq0dcws1fk04&Test=true"
+
+var data = {
+    "Topic": "Azure Functions",
+    "ReqestorFirstName": "Earl", 
+    "ReqestorLastName": "Jones",
+    "ReqestorEmailAddress": "mschray@yahoo.com",
+    "RequestedConversation": "How to migrate from Azure Functions to MVC and Web API site.",
+    "RequestedDayHalf": "Afternoon",
+    "IsTest": "true"
+}
+
+// var request = new XMLHttpRequest();
+// request.open('POST', url, true);
+// request.setRequestHeader('Content-Type', 'application/json; charset=utf-8; charset=UTF-8');
+// request.send(data);
+
+console.log(data);
+
+$.ajax({
+  type: 'POST',
+  url: url,
+  data: data
+}).done(function( result ) {
+    console.log('allo', result);
+  });

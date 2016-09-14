4ae08bb3e9ebb2c1104c34d26fb18f788df99e0b
Tobias Wright <tobias.wright@gmail.com>
2016-09-14 09:36:36 -0700

    added styling and form

--- entry.js
+++ entry.js
@@ -1,33 +1,31 @@
-require("./style.css");
 $ = require("jquery");
 
-// console.log(require("./content.js"));
+$(".submit").click( () => {
+    var url = "https://startupcalendarhelper.azurewebsites.net/api/RequestTopicExpert?code=6yy62ob12opbsym3ombgkeudrq0dcws1fk04"
 
-var url = "https://startupcalendarhelper.azurewebsites.net/api/RequestTopicExpert?code=6yy62ob12opbsym3ombgkeudrq0dcws1fk04"
+    var data = {
+        "Topic": $("#topic").val(),
+        "ReqestorFirstName": $("#first").val(), 
+        "ReqestorLastName": $("#last").val(),
+        "ReqestorEmailAddress": $("#email").val(),
+        "RequestedConversation": $("#queston").val(),
+        "RequestedDayHalf": $("input[name=time]:checked").val(),
+        "IsTest": "true"
+    }
 
-var data = {
-    "Topic": "Azure Functions",
-    "ReqestorFirstName": "Earl", 
-    "ReqestorLastName": "Jones",
-    "ReqestorEmailAddress": "mschray@yahoo.com",
-    "RequestedConversation": "How to migrate from Azure Functions to MVC and Web API site.",
-    "RequestedDayHalf": "Afternoon",
-    "IsTest": "true"
-}
+    $(".loader").css({"display": "block"})
 
-// var request = new XMLHttpRequest();
-// request.open('POST', url, true);
-// request.setRequestHeader('Content-Type', 'application/json; charset=utf-8; charset=UTF-8');
-// request.send(data);
+    console.log(data);
 
-console.log(data);
-
-$.ajax({
-  type: 'POST',
-  url: url,
-  data: JSON.stringify(data),
-  contentType : "application/json; charset=utf-8",
-  dataType: "json",
-}).done(function( result ) {
-    console.log('allo', result);
-  });
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: JSON.stringify(data),
+      contentType : "application/json; charset=utf-8",
+      dataType: "json",
+    }).done(function( result ) {
+        console.log(result)
+        $(".loader").css({"display": "none"})
+        $(".complete").css({"display": "block"})
+      });
+})
